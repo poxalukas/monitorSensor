dd8ece2995445b9438c3483d3ec521f185bcde9c
lucas-silva-aa <130592625+lucas-silva-aa@users.noreply.github.com>
2024-04-27 17:40:05 -0300

    feat:inclusao de funcao que busca ativos

--- src/main/java/com/fiesc/monitor/repository/SensorRepository.java
+++ src/main/java/com/fiesc/monitor/repository/SensorRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Long>, JpaSpecificationExecutor<Sensor> {
+    List<Sensor> findByStatus(boolean status);
+
 }
