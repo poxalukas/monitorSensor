5668b4e017290190e88daa205c1abf7a3aed748c
lucas-silva-aa <130592625+lucas-silva-aa@users.noreply.github.com>
2024-04-27 17:40:41 -0300

    Create DadosSensorDTO.java

---
+++ src/main/java/com/fiesc/monitor/dto/DadosSensorDTO.java
@@ -0,0 +1,18 @@
+package com.fiesc.monitor.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class DadosSensorDTO {
+
+    private Long data;
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Timestamp timestamp;
+}
