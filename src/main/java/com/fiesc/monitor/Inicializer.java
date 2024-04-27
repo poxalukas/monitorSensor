37ceec563ac2fd0dcdf2cb63195559c6010e5963
lucas-silva-aa <130592625+lucas-silva-aa@users.noreply.github.com>
2024-04-27 17:40:44 -0300

    Create Inicializer.java

---
+++ src/main/java/com/fiesc/monitor/Inicializer.java
@@ -0,0 +1,19 @@
+package com.fiesc.monitor;
+
+import com.fiesc.monitor.service.MonitorService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Inicializer {
+
+    @Autowired
+    private MonitorService service;
+
+
+    @PostConstruct
+    public void postConstruct(){
+        service.monitorEndpoint();
+    }
+}
