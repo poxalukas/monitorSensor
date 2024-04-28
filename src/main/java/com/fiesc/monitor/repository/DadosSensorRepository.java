4eaf04bce21e336e638a1609008ff7100f280c76
lucas-silva-aa <130592625+lucas-silva-aa@users.noreply.github.com>
2024-04-28 12:51:05 -0300

    inclusao de busca
    implementação de busca os ultimos 100 valores de um sensor

--- src/main/java/com/fiesc/monitor/repository/DadosSensorRepository.java
+++ src/main/java/com/fiesc/monitor/repository/DadosSensorRepository.java
@@ -1,10 +1,17 @@
 package com.fiesc.monitor.repository;
 
 import com.fiesc.monitor.entity.DadosSensor;
+import com.fiesc.monitor.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DadosSensorRepository extends JpaRepository<DadosSensor, Long>, JpaSpecificationExecutor<DadosSensor> {
+
+    List<DadosSensor> findFirst100BySensorOrderByTimestampDesc(Long sensorId);
 }
